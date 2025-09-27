1ebd5add2769630beddd668daf2d9ed8370751ef
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-09-27 23:04:29 +0500

    removed privacy page; users view and controller

---
+++ UserManagementWebapp/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserManagementWebapp.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
